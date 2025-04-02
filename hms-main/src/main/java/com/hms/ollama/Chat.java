69f7b6b6f7f1687bb9b325b3f9940ea84de7df65
Sillage <15286891906@163.com>
2025-04-02 21:53:58 +0800

    正式上线改造

--- hms-main/src/main/java/com/hms/ollama/Chat.java
+++ hms-main/src/main/java/com/hms/ollama/Chat.java
@@ -1,21 +1,21 @@
-package com.hms.ollama;
-
-import io.github.amithkoujalgi.ollama4j.core.OllamaAPI;
-import io.github.amithkoujalgi.ollama4j.core.models.OllamaResult;
-import io.github.amithkoujalgi.ollama4j.core.utils.OptionsBuilder;
-
-import java.io.IOException;
-
-public class Chat {
-    private static String host = "http://localhost:11434/";
-    private static String model = "qwen2";
-
-    public static String generate(String msg) throws Exception {
-
-        OllamaAPI ollamaAPI = new OllamaAPI(host);
-
-        OllamaResult result =ollamaAPI.generate(model, msg, new OptionsBuilder().build());
-
-        return result.getResponse();
-    }
-}
+//package com.hms.ollama;
+//
+//import io.github.amithkoujalgi.ollama4j.core.OllamaAPI;
+//import io.github.amithkoujalgi.ollama4j.core.models.OllamaResult;
+//import io.github.amithkoujalgi.ollama4j.core.utils.OptionsBuilder;
+//
+//import java.io.IOException;
+//
+//public class Chat {
+//    private static String host = "http://localhost:11434/";
+//    private static String model = "qwen2";
+//
+//    public static String generate(String msg) throws Exception {
+//
+//        OllamaAPI ollamaAPI = new OllamaAPI(host);
+//
+//        OllamaResult result =ollamaAPI.generate(model, msg, new OptionsBuilder().build());
+//
+//        return result.getResponse();
+//    }
+//}
--- hms-main/src/main/java/com/hms/quartz/task/SendMaillTask.java
+++ hms-main/src/main/java/com/hms/quartz/task/SendMaillTask.java
@@ -1,51 +1,51 @@
-package com.hms.quartz.task;
-
-import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONArray;
-import com.hms.common.utils.hms.CommonUtil;
-import com.hms.common.utils.hms.EmailUtil;
-import com.hms.common.utils.hms.MDUtil;
-import com.hms.common.utils.hms.UtilConstants;
-import com.hms.ollama.Chat;
-import com.hms.util.HttpConnection;
-import org.springframework.stereotype.Component;
-
-import java.util.Map;
-
-@Component("SendMaillTask")
-public class SendMaillTask {
-
-    private static CommonUtil util = new CommonUtil();
-    public void task() throws Exception {
-        String gd = getGDAPI();//获取明天天气
-        String gptbody = getgptMsg(gd);//通过qwen2获取信息
-        String html = MDUtil.mdtohtml(gptbody);
-        //发送邮件
-        EmailUtil.sendEmail(UtilConstants.MaillSender.we, "明日天气预报，请查收~", html);
-    }
-    /**
-     * 获取明天的天气
-     * @throws Exception
-     */
-    private String getGDAPI() throws Exception {
-        //url为高德天气预报服务的url，详情请参阅：https://console.amap.com/dev/key/app
-        String url = "https://restapi.amap.com/v3/weather/weatherInfo?key=9073e847b0ab5658da7bd99b6de16f34&city=110114&output=JSOM&extensions=all";
-//        new HttpConnection();
-        String msg = HttpConnection.sendGET(url);
-        System.out.println("高德返回消息："+msg);
-        Map map = util.initMap(msg);
-        String infocode = util.initstr(map.get("infocode"));
-        if(!"10000".equals(infocode)) {
-            throw new Exception("高德请求异常,"+msg);
-        }
-        JSONArray forecasts = (JSONArray) JSON.parse(util.initstr(map.get("forecasts")));
-        Map info = util.initMap(forecasts.get(0));
-        JSONArray casts = (JSONArray)JSON.parse(util.initstr(info.get("casts")));
-        String tomorrow = util.initstr(casts.get(1));
-        return tomorrow;
-    }
-    private String getgptMsg(String data) throws Exception {
-        String msg = Chat.generate("收信人昵称:肉崽崽;我的昵称:崔崔;收信人身份:女朋友;请用中文讲述天气及注意事项，并问好。生成内容请遵循markdown语法。以下是明天的天气信息："+data);
-        return msg;
-    }
-}
+//package com.hms.quartz.task;
+//
+//import com.alibaba.fastjson2.JSON;
+//import com.alibaba.fastjson2.JSONArray;
+//import com.hms.common.utils.hms.CommonUtil;
+//import com.hms.common.utils.hms.EmailUtil;
+//import com.hms.common.utils.hms.MDUtil;
+//import com.hms.common.utils.hms.UtilConstants;
+//import com.hms.ollama.Chat;
+//import com.hms.util.HttpConnection;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Map;
+//
+//@Component("SendMaillTask")
+//public class SendMaillTask {
+//
+//    private static CommonUtil util = new CommonUtil();
+//    public void task() throws Exception {
+//        String gd = getGDAPI();//获取明天天气
+//        String gptbody = getgptMsg(gd);//通过qwen2获取信息
+//        String html = MDUtil.mdtohtml(gptbody);
+//        //发送邮件
+//        EmailUtil.sendEmail(UtilConstants.MaillSender.we, "明日天气预报，请查收~", html);
+//    }
+//    /**
+//     * 获取明天的天气
+//     * @throws Exception
+//     */
+//    private String getGDAPI() throws Exception {
+//        //url为高德天气预报服务的url，详情请参阅：https://console.amap.com/dev/key/app
+//        String url = "https://restapi.amap.com/v3/weather/weatherInfo?key=9073e847b0ab5658da7bd99b6de16f34&city=110114&output=JSOM&extensions=all";
+////        new HttpConnection();
+//        String msg = HttpConnection.sendGET(url);
+//        System.out.println("高德返回消息："+msg);
+//        Map map = util.initMap(msg);
+//        String infocode = util.initstr(map.get("infocode"));
+//        if(!"10000".equals(infocode)) {
+//            throw new Exception("高德请求异常,"+msg);
+//        }
+//        JSONArray forecasts = (JSONArray) JSON.parse(util.initstr(map.get("forecasts")));
+//        Map info = util.initMap(forecasts.get(0));
+//        JSONArray casts = (JSONArray)JSON.parse(util.initstr(info.get("casts")));
+//        String tomorrow = util.initstr(casts.get(1));
+//        return tomorrow;
+//    }
+//    private String getgptMsg(String data) throws Exception {
+//        String msg = Chat.generate("收信人昵称:肉崽崽;我的昵称:崔崔;收信人身份:女朋友;请用中文讲述天气及注意事项，并问好。生成内容请遵循markdown语法。以下是明天的天气信息："+data);
+//        return msg;
+//    }
+//}
